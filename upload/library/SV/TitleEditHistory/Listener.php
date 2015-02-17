1c5f1ce05a984be1216c83f770a98593b92b93bd
Xon <tazipper1@hotmail.com>
2015-02-17 10:39:00 +0800

    Trigger ContentType Cache rebuild

--- upload/library/SV/TitleEditHistory/Listener.php
+++ upload/library/SV/TitleEditHistory/Listener.php
@@ -37,6 +37,7 @@ class SV_TitleEditHistory_Listener
             SV_TitleEditHistory_Install::renameColumn('xf_thread','last_edit_user_id', 'thread_title_last_edit_user_id', 'int not null default 0');
         }
 
+        XenForo_Model::create('XenForo_Model_ContentType')->rebuildContentTypeCache();
         return true;
     }
 
@@ -54,7 +55,7 @@ class SV_TitleEditHistory_Listener
             WHERE xf_content_type_field.field_value = '".self::AddonNameSpace."_EditHistoryHandler_Thread'
         ");
 
-
+        XenForo_Model::create('XenForo_Model_ContentType')->rebuildContentTypeCache();
         return true;
     }
 
