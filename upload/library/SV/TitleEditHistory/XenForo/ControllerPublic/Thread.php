256dee83ce011b9a4667d7ccf915d67308d0f714
Xon <tazipper1@hotmail.com>
2015-02-27 12:55:03 +0800

    Suppress moderator logging of when thread title editing metadata.

--- upload/library/SV/TitleEditHistory/XenForo/ControllerPublic/Thread.php
+++ upload/library/SV/TitleEditHistory/XenForo/ControllerPublic/Thread.php
@@ -8,4 +8,10 @@ class SV_TitleEditHistory_XenForo_ControllerPublic_Thread  extends XFCP_SV_Title
         $this->_request->setParam('content_id', $this->_input->filterSingle('thread_id', XenForo_Input::UINT));
         return $this->responseReroute('XenForo_ControllerPublic_EditHistory', 'index');
     }
+
+	protected function _updateModeratorLogThreadEdit(array $thread, XenForo_DataWriter_Discussion_Thread $dw, array $skip = array())
+	{
+        $skip += array('thread_title_last_edit_date', 'thread_title_last_edit_user_id', 'thread_title_edit_count');
+        parent::_updateModeratorLogThreadEdit($thread, $dw, $skip);
+    }
 }
